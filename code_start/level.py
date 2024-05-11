2636715e35174012422e47c3f49ac3cdaf5b87f2
Andrii <shurgaiandrew335@gmail.com>
2024-05-11 19:47:11 +0300

    Adding moving objects

--- code_start/level.py
+++ code_start/level.py
@@ -1,5 +1,5 @@
 from settings import *
-from sprites import Sprite
+from sprites import Sprite, MovingSprite
 from player import Player
 
 class Level:
@@ -13,12 +13,32 @@ class Level:
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
+
+        # tiles
         for x, y, surface in tmx_map.get_layer_by_name('Terrain').tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), surface, (self.all_sprites, self.collision_sprites))
 
+        # objects
         for obj in tmx_map.get_layer_by_name('Objects'):
             if obj.name == 'player':
                 Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+        
+        # moving objects
+        for move_obj in tmx_map.get_layer_by_name('Moving Objects'):
+            if move_obj.name == 'helicopter':
+                if move_obj.width > move_obj.height: # horizontal movement
+                    move_direction = 'x'
+                    start_pos = (move_obj.x, move_obj.y + move_obj.height / 2)
+                    end_pos =  (move_obj.x + move_obj.width, move_obj.y + move_obj.height / 2)
+                else: # vertical movement
+                    move_direction = 'y'
+                    start_pos = (move_obj.x + move_obj.width / 2, move_obj.y)
+                    end_pos = (move_obj.x + move_obj.width / 2, move_obj.y + move_obj.height)
+                speed = move_obj.properties['speed']
+                MovingSprite(self.all_sprites, start_pos, end_pos, move_direction, speed)
+                
+
+
 
     def run(self, delta_time):
         self.display_surface.fill("black")
--- code_start/sprites.py
+++ code_start/sprites.py
@@ -1,9 +1,14 @@
 from settings import *
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surface, groups):
+    def __init__(self, pos, surface=pygame.Surface((TILE_SIZE, TILE_SIZE)), groups=None):
         super().__init__(groups)
-        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        self.image = surface
         self.image.fill('gray')
         self.rect = self.image.get_frect(topleft=pos)
         self.old_rect = self.rect.copy()
+
+class MovingSprite(Sprite):
+    def __init__(self, groups, start_pos, end_pos, move_direction, speed):
+        surface = pygame.Surface((200, 50))
+        super().__init__(start_pos, surface, groups)
